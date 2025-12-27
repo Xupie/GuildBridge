5b0caeb0d32ebf99258b40e6df40db8f1243b594
xupie <81492239+Xupie@users.noreply.github.com>
2025-12-27 18:57:55 +0200

    fix regex for no rank bot

--- src/main/java/com/github/xupie/guildbridge/commands/TestBridgeCommand.java
+++ src/main/java/com/github/xupie/guildbridge/commands/TestBridgeCommand.java
@@ -1,14 +1,15 @@
 package com.github.xupie.guildbridge.commands;
 
+import com.github.xupie.guildbridge.config.ModConfig;
 import com.github.xupie.guildbridge.features.GuildBridge;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
 
 @Environment(EnvType.CLIENT)
@@ -20,10 +21,22 @@ public final class TestBridgeCommand {
         );
     }
 
-    private static int broadcast(CommandContext<FabricClientCommandSource> context) throws CommandSyntaxException {
-        GuildBridge.register(Text.literal("§2Guild > §b[MVP§c+§b] IsleofDucks §e[STAFF]§f: Xupie: b"), true);
+    private static int broadcast(CommandContext<FabricClientCommandSource> context) {
+        String botIGN = ModConfig.HANDLER.instance().botIGN;
+
+        Text processed = GuildBridge.register(
+            Text.literal("§2Guild > §b[MVP§c+§b] " + botIGN + " §e[STAFF]§f: Xupie: b"),
+            true
+        );
+
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client.player != null) {
+            client.inGameHud.getChatHud().addMessage(processed);
+        }
+
         return Command.SINGLE_SUCCESS;
     }
+
 }
 
 
--- src/main/java/com/github/xupie/guildbridge/commands/TestGuildCommand.java
+++ src/main/java/com/github/xupie/guildbridge/commands/TestGuildCommand.java
@@ -8,6 +8,7 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
 
 @Environment(EnvType.CLIENT)
@@ -20,7 +21,16 @@ public final class TestGuildCommand {
     }
 
     private static int broadcast(CommandContext<FabricClientCommandSource> context) {
-        GuildBridge.register(Text.literal("§2Guild > §a[VIP§6+§a] Xupie §e[EMBER]§f: b"), true);
+        Text processed = GuildBridge.register(
+            Text.literal("§2Guild > §a[VIP§6+§a] Xupie §e[EMBER]§f: b"),
+            true
+        );
+
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client.player != null) {
+            client.inGameHud.getChatHud().addMessage(processed);
+        }
+
         return Command.SINGLE_SUCCESS;
     }
 }
